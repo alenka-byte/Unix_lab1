6cb890e769ad266bc2b1435c34feec5283b90224
alenka-byte <alenkaivas6@gmail.com>
2025-09-23 15:48:16 +0700

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,7 @@
+//&Output: "It worked!"
+#include <iostream> 
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ lab1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh 
+
+file=$1
+
+if [ ! -f "$file" ]; then
+   echo "Error: File '$file'  not found"
+   exit 1
+fi
+
+if grep -q "&Output:" "$file"; then
+   output=$(grep "&Output:" "$file" | cut -d: -f2- | xargs)
+else
+   exit 1;
+fi
+
+tmp=$(mktemp -d) || exit 1
+
+cp "$file" "$tmp/"
+cd "$tmp" || exit 1
+
+echo "Compiling $file -> $output"
+if g++ "$(basename "$file")" -o "$output"; then
+   cp "$output " "$(dirname "$file")/"
+   echo "Success: Output copied to $(dirname "$file")/$output"
+else
+   echo "Complilation failed"
+   exit 1
+fi
